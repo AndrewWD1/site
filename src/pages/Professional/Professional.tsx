adfbdd0381ad20c66a254b67395055ad9e05b898
Andrew Doumont <andrew.doumont@gmail.com>
2020-12-29 18:31:46 -0700

    update

--- src/pages/Professional/Professional.tsx
+++ src/pages/Professional/Professional.tsx
@@ -15,10 +15,10 @@ const Professional = () => (
     <Container style={ContainerStyle}>
       <h2>Professional</h2>
       <p>
-        I'm a first-year grad student in the Mathematics department at
-        CU-Boulder. My math interests are generally in Algebra and Topology. I
-        received my Bachelors degree in Mathematics from West Point in 2015 and
-        started graduate school at CU in 2020.
+        I'm a first year grad student in the Mathematics department at
+        CU-Boulder. My math interests are generally in Algebra and Topology, but
+        I also find interests in many areas outside of mathematics and hope to
+        be able to bridge some of those gaps eventually.
       </p>
     </Container>
 
@@ -26,30 +26,34 @@ const Professional = () => (
       <h2>Teaching</h2>
       <GeneralTable
         head={["Semester", "Course", "Sections"]}
-        rows={[["FALL 2020", "Calculus II TA (MATH-2300)", "003, 006, 009"]]}
+        rows={[
+          ["FALL 2020", "Calculus II TA (MATH-2300)", "003, 006, 009"],
+          ["SPRING 2021", "Calculus I TA (MATH-2300)", ""],
+        ]}
       />
+      <br />
+      <div style={{ borderTop: "1px solid black" }}></div>
+      <br />
+      <h2>Tutoring</h2>
+      <ul>
+        <li>I can tutor elementary through undergraduate mathematics.</li>
+        <li>My rate is $30/hour.</li>
+        <li>
+          My preferance is to tutor evenings Sunday-Friday, but we can work out
+          something mutually beneficial.
+        </li>
+        <li>
+          Contact me at <strong>andrew.doumont@colorado.edu</strong>
+        </li>
+      </ul>
     </Container>
 
     <Container style={ContainerStyle}>
-      <h2>Solutions to Math Textbook Problems</h2>I was reviewing "A Book of
-      Abstract Algebra" by Charles C. Pinter and positing solutions{" "}
-      <a href="https://github.com/AndrewWD1/Math/tree/master/Practice_Problems/A_Book_Of_Abstract_Algebra">
-        <strong>here.</strong>
-      </a>
-      This text can be found for free on the internet archive{" "}
-      <a href="https://archive.org/details/charles-c-pinter-a-book-of-abstract-algebra/mode/2up">
-        <strong>here.</strong>
-      </a>{" "}
-      Please feel free to make a pull request on github to add solutions. I
-      think this could be a really valuable resource for self-learners.
-    </Container>
-
-    <Container style={ContainerStyle}>
-      <h2>Blog</h2>I am also starting a{" "}
-      <a href="https://andrewdoumont.netlify.app">blog</a> that I hope to be
-      more active with in the coming months. It'll touch on topics of math,
-      programming, and other hobies of mine. It's built with Gatsby and hosted
-      of Netlify.
+      <h2>Blog</h2>I have a{" "}
+      <a href="https://andrewdoumont.netlify.app">blog.</a> It's filled with
+      many half finished thoughts and ideas. I hope to write more about my
+      interests of math, philosophy, and generally things happening in my life.
+      It's built with Gatsby and hosted of Netlify.
     </Container>
   </Template>
 );
