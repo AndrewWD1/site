896dd1ff470832a2328129e33c8f13f449e8bb6d
Andrew Doumont <44379480+AndrewWD1@users.noreply.github.com>
2023-05-25 19:17:21 +0000

    Update 7 files

--- next.config.js
+++ next.config.js
@@ -1,10 +1,24 @@
+const frontmatterPlugin = () => (tree) => {
+  // This is removing the frontmatter from mdx files
+  // so that the info can be used but doesn't get
+  // displayed in the page
+  if (tree.children[0].type === "thematicBreak") {
+    const firstHeadingIndex = tree.children.findIndex(
+      (t) => t.type === "heading"
+    );
+    if (firstHeadingIndex !== -1) {
+      tree.children.splice(0, firstHeadingIndex + 1);
+    }
+  }
+};
+
 const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
   options: {
     // If you use remark-gfm, you'll need to use next.config.mjs
     // as the package is ESM only
     // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [],
+    remarkPlugins: [frontmatterPlugin],
     rehypePlugins: [],
     // If you use `MDXProvider`, uncomment the following line.
     providerImportSource: "@mdx-js/react",
---
+++ pages/blog/index.js
@@ -0,0 +1,76 @@
+import Nav from "../../components/nav";
+import styles from "../../styles/Home.module.css";
+import Link from "next/link";
+
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const removeFileExtension = (str) => str.replace(/\.(.*)/g, "");
+
+export default function Home({ posts }) {
+  return (
+    <>
+      <Nav />
+      <main>
+        <div>
+          <h1>Blog Entries</h1>
+          <ul>
+            {posts.map((post) => {
+              return (
+                <li key={post.id}>
+                  <Link href={`/blog/${removeFileExtension(post.id)}`}>
+                    {post.title}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </main>
+    </>
+  );
+}
+
+const postsDirectory = path.join(process.cwd(), "pages/blog");
+
+export async function getStaticProps() {
+  const fileNames1 = fs.readdirSync(postsDirectory);
+  const fileNames = fileNames1.filter((x) => x != "index.js");
+
+  const allPostsData = fileNames.map((fileName) => {
+    // Remove ".md" from file name to get id
+    const id = fileName.replace(/\.md$/, "");
+
+    // Read markdown file as string
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
+
+    // Combine the data with the i
+    return {
+      id,
+      ...matterResult.data,
+    };
+  });
+
+  //Sort posts by date
+  const sortedPosts = allPostsData.sort((a, b) => {
+    if (a.date < b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+
+  //Fix date serialization for json
+  let posts = JSON.parse(JSON.stringify(sortedPosts));
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
